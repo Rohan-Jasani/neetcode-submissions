f3f82021590c3fdec8d89869037eee0c592bf768
Rohan-Jasani <64419315+Rohan-Jasani@users.noreply.github.com>
2026-08-27 16:46:42 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,18 @@
+# from bisect import bisect_left
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len( nums )
+        seen = {}
+        for i, num in enumerate( nums ):
+            complement = target - num
+
+            if complement in seen: return [ seen[complement], i ]
+
+            seen[num] = i
+        
+        return []
+
+
+
+
+
