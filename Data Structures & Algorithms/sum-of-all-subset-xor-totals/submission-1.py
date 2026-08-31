ebe52487f508b97c770b58e9091c8500594827c2
Rohan-Jasani <64419315+Rohan-Jasani@users.noreply.github.com>
2026-08-31 13:30:50 +0530

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        n = len( nums )
+        ans = 0
+        def dfs( path_XOR, i ):
+            nonlocal ans
+            if i == n: 
+                ans += path_XOR
+                return
+
+            
+            # dont choose nums[i]
+            dfs( path_XOR, i+1)
+
+            # choose nums[i]
+            path_XOR ^= nums[i]
+            dfs( path_XOR, i+1 )
+            path_XOR ^= nums[i]
+        
+        dfs( 0, 0 )
+        return ans
+        '''
+        n = len( nums )
+        ans = 0
+        for mask in range( 0, 2**n ):
+            total = 0
+            for j in range( 0, n ):
+                if mask & ( 1<<j ):
+                    total ^= nums[j]
+            # print( mask, total )
+            ans += total
+        return ans
+        '''
